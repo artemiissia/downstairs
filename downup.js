8be9ce14e7e6baaa0802cee63ac9d2a7c542924f
Artemisia Luk <artemisialuk@Artemisias-MacBook-Air.local>
2019-10-27 23:01:02 -0400

    lease enter the commit message for your changes. Lines starting

---
+++ downup.js
@@ -0,0 +1,11 @@
+document.getElementById("nav-explore").addEventListener("click", clickFormat);
+document.getElementById("nav-inspiratoin").addEventListener("click", clickFormat);
+document.getElementById("nav-process").addEventListener("click", clickFormat);
+document.getElementById("nav-inuse").addEventListener("click", clickFormat);
+
+
+function clickFormat() {
+
+    //remove and add class
+
+}
